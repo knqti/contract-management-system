8571d156f2f4ae79f6086fa2b479f8dd73fdfebc
knqti <166448899+knqti@users.noreply.github.com>
2025-05-24 22:37:36 -0700

    Add create_table query

--- queries.py
+++ queries.py
@@ -3,4 +3,15 @@ def select_all(cursor: object):
     results = cursor.fetchall()
     for row in results:
         print(row)
+
+def create_table(cursor: object):
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Contracts(
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            vendor TEXT,
+            cost REAL,
+            pay_cycle TEXT,
+            expires_on TEXT)
+    ''')
         
